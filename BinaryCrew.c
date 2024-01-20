213880650db161fb29e480ccc6103d4570560260
Shrey Bansal <151784865+ShreyB-007@users.noreply.github.com>
2024-01-20 12:08:47 +0530

    Update BinaryCrew.c

--- BinaryCrew.c
+++ BinaryCrew.c
@@ -8,6 +8,7 @@ void main()
     long long contact;
     char title[4];
     int qualification;
+    int a=0;
     printf("Please enter your first name- ");
     scanf("%s",name1);
     printf("Please enter your last name- ");
@@ -26,20 +27,19 @@ void main()
     {
         snprintf(title,sizeof(title),"Ms.");
     }
-    printf("Please enter your qualification:\nFor 10th pass press 1.\nFor 12th pass press 2.");
-    scanf("%d",&qualification);
-    switch(qualification)
+    while(a=0)
     {
-        case 1:
+        printf("Please enter your qualification:\nFor 10th pass press 1.\nFor 12th pass press 2.");
+        scanf("%d",&qualification);
+        switch(qualification)
         {
-
+            case 1:
+            a=1;
+            break;
+            case 2:
+            a=2;
+            break;
+            default:
+            printf("Please enter a valid response.");
         }
-        break;
-        case 2:
-        {
-
-        }
-        break;
-        default:
-        printf("Please enter a valid response.");
     }
