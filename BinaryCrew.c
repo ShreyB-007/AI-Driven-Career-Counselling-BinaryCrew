2a068cdc6965a6b63ce724494500a342cc4eb176
Shrey Bansal <151784865+ShreyB-007@users.noreply.github.com>
2024-01-20 20:28:55 +0530

    Update BinaryCrew.c

--- BinaryCrew.c
+++ BinaryCrew.c
@@ -117,6 +117,286 @@ void main()
         {
             NM=0;
         }
-        
+        printf("Have you ever considered going into the intricacies of human development?(Y/N)");
+        scanf("%s",&M);
+        if(M=='Y')
+        {
+            MS+=1;
+            M=0;
+        }
+        else
+        {
+            M=0;
+        }
+        printf("Are you interested in conducting research on animal behavior?(Y/N)");
+        scanf("%s",&M);
+        if(M=='Y')
+        {
+            MS+=1;
+            M=0;
+        }
+        else
+        {
+            M=0;
+        }
+        printf("Are you keen on exploring the development of a novel medical treatment?(Y/N)");
+        scanf("%s",&M);
+        if(M=='Y')
+        {
+            MS+=1;
+            M=0;
+        }
+        else
+        {
+            M=0;
+        }
+        printf("Are you interested in engaging in biological research?(Y/N)");
+        scanf("%s",&M);
+        if(M=='Y')
+        {
+            MS+=1;
+            M=0;
+        }
+        else
+        {
+            M=0;
+        }
+        printf("Are you interested in assisting people with mental health challenges?(Y/N)");
+        scanf("%s",&M);
+        if(M=='Y')
+        {
+            MS+=1;
+            M=0;
+        }
+        else
+        {
+            M=0;
+        }
+        printf("Do you have an interest in managing customer bank transactions?(Y/N)");
+        scanf("%s",&CM);
+        if(CM=='Y')
+        {
+            CMS+=1;
+            CM=0;
+        }
+        else
+        {
+            CM=0;
+        }
+        printf("Do you have an interest in maintaining employee records?(Y/N)");
+        scanf("%s",&CM);
+        if(CM=='Y')
+        {
+            CMS+=1;
+            CM=0;
+        }
+        else
+        {
+            CM=0;
+        }
+        printf("Do you have an interest in assisting people with financial management?(Y/N)");
+        scanf("%s",&CM);
+        if(CM=='Y')
+        {
+            CMS+=1;
+            CM=0;
+        }
+        else
+        {
+            CM=0;
+        }
+        printf("Do you have an interest in aiding companies in identifying financial issues?(Y/N)");
+        scanf("%s",&CM);
+        if(CM=='Y')
+        {
+            CMS+=1;
+            CM=0;
+        }
+        else
+        {
+            CM=0;
+        }
+        printf("Are you interested in collecting survey data and analyzing it?(Y/N)");
+        scanf("%s",&CM);
+        if(CM=='Y')
+        {
+            CMS+=1;
+            CM=0;
+        }
+        else
+        {
+            CM=0;
+        }
+        printf("Do you have an interest in managing a shop?(Y/N)");
+        scanf("%s",&C);
+        if(C=='Y')
+        {
+            CS+=1;
+            C=0;
+        }
+        else
+        {
+            C=0;
+        }
+        printf("Do you have an interest in managing heavy maintenance departments?(Y/N)");
+        scanf("%s",&C);
+        if(C=='Y')
+        {
+            CS+=1;
+            C=0;
+        }
+        else
+        {
+            C=0;
+        }
+        printf("Do you have an interest in overseeing the operations of a hotel?(Y/N)");
+        scanf("%s",&C);
+        if(C=='Y')
+        {
+            CS+=1;
+            C=0;
+        }
+        else
+        {
+            C=0;
+        }
+        printf("Do you have an interest in taking on the role of a company secretary?(Y/N)");
+        scanf("%s",&C);
+        if(C=='Y')
+        {
+            CS+=1;
+            C=0;
+        }
+        else
+        {
+            C=0;
+        }
+        printf("Do you have an interest in managing the online presence of your company?(Y/N)");
+        scanf("%s",&C);
+        if(C=='Y')
+        {
+            CS+=1;
+            C=0;
+        }
+        else
+        {
+            C=0;
+        }
+        printf("Do you possess strong problem-solving skills?(Y/N)");
+        scanf("%s",&H);
+        if(H=='Y')
+        {
+            HS+=1;
+            H=0;
+        }
+        else
+        {
+            H=0;
+        }
+        printf("Are you interested in interpreting evidence from texts, artifacts, and other materials spanning different time periods?(Y/N)");
+        scanf("%s",&H);
+        if(H=='Y')
+        {
+            HS+=1;
+            H=0;
+        }
+        else
+        {
+            H=0;
+        }
+        printf("Do you have an interest in studying past events?(Y/N)");
+        scanf("%s",&H);
+        if(H=='Y')
+        {
+            HS+=1;
+            H=0;
+        }
+        else
+        {
+            H=0;
+        }
+        printf("Are you interested in sharing pertinent information with a broad audience via television, radio, or newspapers?(Y/N)");
+        scanf("%s",&H);
+        if(H=='Y')
+        {
+            HS+=1;
+            H=0;
+        }
+        else
+        {
+            H=0;
+        }
+        printf("Do you have an interest in acquiring knowledge of new languages?(Y/N)");
+        scanf("%s",&H);
+        if(H=='Y')
+        {
+            HS+=1;
+            H=0;
+        }
+        else
+        {
+            H=0;
+        }
+        printf("Are you interested in analyzing crime scenes for the presence of fingerprints?(Y/N)");
+        scanf("%s",&BE);
+        if(BE=='Y')
+        {
+            BES+=1;
+            BE=0;
+        }
+        else
+        {
+            BE=0;
+        }
+        printf("Do you have interest in pursuing a career in research field?(Y/N)");
+        scanf("%s",&BE);
+        if(BE=='Y')
+        {
+            BES+=1;
+            BE=0;
+        }
+        else
+        {
+            BE=0;
+        }
+        printf("Do you have a natural curiosity for both the physical and life sciences in your academic pursuits?(Y/N)");
+        scanf("%s",&BE);
+        if(BE=='Y')
+        {
+            BES+=1;
+            BE=0;
+        }
+        else
+        {
+            BE=0;
+        }
+        printf("Are you intrigued by the study of viruses, cells, and various chemical substances?(Y/N)");
+        scanf("%s",&BE);
+        if(BE=='Y')
+        {
+            BES+=1;
+            BE=0;
+        }
+        else
+        {
+            BE=0;
+        }
+        printf("Are you drawn to academic disciplines that encompass a combination of mathematical and scientific principles?(Y/N)");
+        scanf("%s",&BE);
+        if(BE=='Y')
+        {
+            BES+=1;
+            BE=0;
+        }
+        else
+        {
+            BE=0;
+        }
+        printf("We are glad to inform you that the survey is over now. Thank you for your cooperation. Press any key for the results.");
+        scanf("%s",&key);
+        if(b=='Y')
+        {
+        printf("Given your priority list at the start of the survey and the questions you answered during your survey, we have concluded that:");
+        }
     }
 }
